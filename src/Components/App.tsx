f5b3d9300b75e0511e4bb16754dc90550e8b8b39
Oliver Hale <oliver.a.hale@gmail.com>
2023-11-01 15:54:15 +1100

    add very basic scribe logo and description on main page

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,32 +1,29 @@
 import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '../assets/electron-vite.animate.svg'
+// import reactLogo from '../assets/react.svg'
+import scribeLogo from '../assets/quill-svgrepo-com.svg'
 import '../App.css'
-
+// https://iconscout.com/icon/quill-2969118
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://electron-vite.github.io" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <a href="https://oliverhale.internal.halopsa.com" target="_blank">
+          <img src={scribeLogo} className="logo" alt="Scribe Logo" />
+          </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>SCRIBE</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        Streamlining Configuration and Requests Interface for Bulk Efficiency
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <a href="https://github.com/s3781403/halo-scribe" target="_blank">Open Source</a>
       </p>
     </>
   )
